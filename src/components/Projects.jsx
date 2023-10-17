edac599e9c9cb5ce76051098d106553d84b2d4b4
swordmaster2005 <soulmaster9794@gmail.com>
2023-10-17 15:40:28 +0530

    fixed imgs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,14 @@ import { Plinks } from '../data/projectsLink';
 import '../App.css'
 import {BsGithub} from 'react-icons/bs'
 
+import comfyimg from '../assets/comfyimg.png'
+import movies from '../assets/movies.png'
+import userGeneretor from '../assets/userGeneretor.png'
+
 const Projects = () => {
   console.log(Plinks);
+
+  const pictures =[comfyimg,movies ,userGeneretor]
 
   return (
     <div id='projects' className='h-screen pt-20 p-10 sm:p-20 align-elements '>
@@ -12,13 +18,16 @@ const Projects = () => {
 
      <div className='flex flex-col  items-center sm:grid  lg:grid-cols-2 gap-10  pb-[40px]'>
      {Plinks.map((link) => {
-        const { id, title, img, gitHub, Netlify } = link;
+        const { id, title,  gitHub, Netlify } = link;
 
         return (
           <div key={id}  className=''>
             <h3 className='text-2xl sm:text-3xl font-bold inline-block p-3 w-full text-center sm:text-start'>{title}</h3>
             <div className='relative projects__box overflow-hidden projects-shadow rounded-[10px]'>
-              <img src={img} alt={title} className='scale-110 sm:scale-100  '/>
+              <img src={pictures[id]} 
+              
+              
+              alt={title} className='scale-110 sm:scale-100 w-full '/>
 
                <span className='projects__box-links absolute  w-full h-full flex justify-center top-[-100%] items-center  bg-[#a35bf7ae] flex-col gap-3 text-[#fff]'>
                  <a href={gitHub} className='font-bold text-2xl flex items-center gap-3 '><BsGithub className='w-10 h-10'/> GitHub Link</a>
--- src/data/projectsLink.js
+++ src/data/projectsLink.js
@@ -1,22 +1,19 @@
 export const Plinks = [
     {
-      id:1,
+      id:0,
       title: 'Comfy',
-      img: './src/assets/comfyimg.png',
       gitHub: 'https://github.com/Spritmaster/comfyproducts-en',
       Netlify: 'https://comfy-nodirbek.netlify.app/',
     },
     {
-      id:2,
+      id:1,
       title: 'Movies Research',
-      img: './src/assets/movies.png',
       gitHub: 'https://github.com/Spritmaster/Movies',
       Netlify: 'https://movies-by-nodirbek.netlify.app/',
     },
     {
-      id:3,
+      id:2,
       title: 'User Generetor',
-      img: './src/assets/userGeneretor.png',
       gitHub: 'hthttps://github.com/Spritmaster/persons-tablet',
       Netlify: 'https://persontablet.netlify.app/',
     }
